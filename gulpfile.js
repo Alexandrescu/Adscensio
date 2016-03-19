1fdf6780c4055cae6b68d81d46bf717efd2c49fa
Alexandrescu <andrei.alexandrescu@oriel.ox.ac.uk>
2016-03-19 18:25:40 +0200

    Added Angular-Route.

--- gulpfile.js
+++ gulpfile.js
@@ -83,6 +83,7 @@ gulp.task('build', function() {
   moveIndex();
   gulp.src([
       'node_modules/angular/angular.js',
+      'node_modules/angular-route/angular-route.js',
       'node_modules/angular-animate/angular-animate.js',
       'node_modules/angular-aria/angular-aria.js',
       'node_modules/angular-material/angular-material.js',
