951eb58367dbb59bad546726b803f89b3cf790c0
Alexandrescu <andrei.alexandrescu@oriel.ox.ac.uk>
2016-03-21 14:27:38 +0200

    Creating flashCards with buttons.

--- source/app/main.ts
+++ source/app/main.ts
@@ -1,4 +1,4 @@
-var adscension = angular.module('adscension', ['ngMaterial', 'ngRoute']);
+var adscension = angular.module('adscension', ['ngMaterial', 'ngRoute', 'ngMdIcons']);
 
 adscension.config(function($routeProvider, $locationProvider){
     $routeProvider
@@ -30,9 +30,36 @@ adscension.controller('HomeController', function($scope) {
     $scope.text = "Hello World!!!!";
 });
 
-adscension.controller('TargetsController', function($scope) {
-    $scope.text = "Targets Controller";
-});
+adscension.controller('TargetsController', ['targets', '$scope',
+    function(targets: ITargetService, $scope) {
+        $scope.text = targets.getTargets();
+        $scope.inProgress = $scope.text[0].status.inProgress();
+
+        // Serious code
+        $scope.targets = targets.getTargets();
+        $scope.isList = false;
+        $scope.isEdit = false;
+        $scope.isCheck = false;
+
+        $scope.toggleList = function() {
+            $scope.isList = !$scope.isList;
+            $scope.isEdit = false;
+            $scope.isCheck = false;
+        };
+
+        $scope.toggleEdit = function() {
+            $scope.isList = false;
+            $scope.isEdit = !$scope.isEdit;
+            $scope.isCheck = false;
+        };
+
+        $scope.toggleCheck = function() {
+            $scope.isList = false;
+            $scope.isEdit = false;
+            $scope.isCheck = !$scope.isCheck;
+        }
+    }
+]);
 
 adscension.controller('LogController', function($scope) {
     $scope.text = "Log Controller";
@@ -40,4 +67,73 @@ adscension.controller('LogController', function($scope) {
 
 adscension.controller('AnalyticsController', function($scope) {
     $scope.text = "Analytics Controller";
+});
+
+
+// Class declarations
+class Target {
+    name: string;
+    status: Status;
+    taskList: Array<Task>;
+    doneTasks():number {
+        var total:number = 0;
+        for (var task in this.taskList) {
+            if (task.done) {
+                total += 1
+            }
+        }
+        return total;
+    }
+    allTasks(): number {
+        return this.taskList.length;
+    }
+    addTask(task: Task) {
+        this.taskList.push(task);
+    }
+    progress():number {
+        return 77;
+    }
+
+    constructor(name: string) {
+        this.name = name;
+        this.status = new Status();
+        this.taskList = [];
+    }
+}
+class Status {
+    status: number = 0;
+    inProgress(): boolean {
+        return status == 0;
+    }
+    isDone(): boolean {
+        return status == 1;
+    }
+}
+
+// Services
+adscension.factory('targets', function(): ITargetService {
+    class SimpleTargetService implements ITargetService {
+        getTargets():Array<Target> {
+            var disframe: Target = new Target('Disframe');
+            disframe.addTask({
+                units: 1,
+                title: 'Asana',
+                done: false
+            });
+
+            disframe.addTask({
+                units: 2,
+                title: 'Facebook',
+                done: true
+            });
+
+            return [disframe];
+        }
+
+        addTarget(target:Target) {
+        }
+
+    }
+
+    return new SimpleTargetService();
 });
--- source/typings/references.d.ts
+++ source/typings/references.d.ts
@@ -3,3 +3,31 @@
 
 // Our references:
 // declare function andrei(): string;
+
+declare interface ITargetService {
+    getTargets(): Array<Target>;
+    addTarget(target: Target);
+}
+
+declare class Target {
+    name: string;
+    status: Status;
+    taskList: Array<Task>;
+    doneTasks(): number;
+    allTasks(): number;
+    addTask(task: Task);
+    progress(): number;
+    constructor(name: string);
+}
+
+declare class Status {
+    status: number;
+    inProgress(): boolean;
+    isDone(): boolean;
+}
+
+declare class Task {
+    units: number;
+    title: string;
+    done: boolean;
+}
