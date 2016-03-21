7f51ecfbc17ac074623c7c1e48fb70011b767654
Alexandrescu <andrei.alexandrescu@oriel.ox.ac.uk>
2016-03-21 10:35:41 +0200

    Added Taskbar with routes.

--- source/app/main.ts
+++ source/app/main.ts
@@ -1,2 +1,43 @@
-var adscension = angular.module('adscension', ['ngMaterial']);
+var adscension = angular.module('adscension', ['ngMaterial', 'ngRoute']);
 
+adscension.config(function($routeProvider, $locationProvider){
+    $routeProvider
+        .when('/', {
+            templateUrl: '/pages/home.html',
+            controller: 'HomeController'
+        })
+        .when('/targets', {
+            templateUrl: '/pages/targets.html',
+            controller: 'TargetsController'
+        })
+        .when('/log', {
+            templateUrl: '/pages/log.html',
+            controller: 'LogController'
+        })
+        .when('/analytics', {
+            templateUrl: '/pages/analytics.html',
+            controller: 'AnalyticsController'
+        });
+
+    $locationProvider.html5Mode(true);
+});
+
+
+
+
+//Controllers
+adscension.controller('HomeController', function($scope) {
+    $scope.text = "Hello World!!!!";
+});
+
+adscension.controller('TargetsController', function($scope) {
+    $scope.text = "Targets Controller";
+});
+
+adscension.controller('LogController', function($scope) {
+    $scope.text = "Log Controller";
+});
+
+adscension.controller('AnalyticsController', function($scope) {
+    $scope.text = "Analytics Controller";
+});
